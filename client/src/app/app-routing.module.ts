df8dc7d47360055bd0cd09733017d97012d9f1b2
Junior Eteuati <junior.eteuati@gmail.com>
2020-06-01 05:53:35 +1200

    Handle Error for Client

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { MealDetailsComponent } from './shop/meal-details/meal-details.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
