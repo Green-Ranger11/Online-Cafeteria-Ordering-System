e580ad9b898006dd4137ceb98bd57539c02ce7ca
Junior Eteuati <junior.eteuati@gmail.com>
2020-06-03 09:25:59 +1200

    Build Front end for Identity

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,25 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IMeal } from './shared/models/meal';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Cafeteria Ordering System';
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    this.accountService.loginCurrentUser(token).subscribe(
+      () => {
+        console.log('loaded user');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
-      this.basketService.getBasket(basketId).subscribe(() => {
-        console.log('Initialized basket');
-      }, error => {
-        console.log(error);
-      });
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('Initialized basket');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
   }
 }
---
+++ client/src/app/shared/models/address.ts
@@ -0,0 +1,6 @@
+export interface IAddress {
+  firstName: string;
+  lastName: string;
+  building: string;
+  room: string;
+}
